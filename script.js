f2701062e2b91b9a676c434bad1a1d2688e8324f
Nick Sanchez <nicksanchez2371@gmail.com>
2024-01-24 01:07:00 -0600

    cant get javascript form validation to work

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-var password = document.getElementById('password');
-var confirmPassword = document.getElementById('confirmPassword');
+const inputFields = document.querySelectorAll('input');
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirmPassword');
 const form = document.getElementById('form');
 const firstName = document.getElementById('firstName');
 const lastName = document.getElementById('lastName');
@@ -7,8 +8,8 @@ const email = document.getElementById('email');
 const phoneNum = document.getElementById('phone');
 const error = document.getElementById('error');
 
-
-
+let allFieldsValidity = new Array(6).fill(false);
+/*
 form.addEventListener('submit', (e) => {
     let messages = [];
     if (firstName.value === '' || firstName.value == null){
@@ -27,7 +28,7 @@ form.addEventListener('submit', (e) => {
     error.innerText = messages.join(', ')
     }
 } )
-
+*/
 //confirms password
 
 function validatePassword() {
@@ -38,5 +39,52 @@ function validatePassword() {
     }
 }
 
+function validateRequired(e) {
+    if (e.target.value === '') {
+      e.target.parentElement.classList.add('error');
+      e.target.nextElementSibling.textContent = 'This field is required';
+      return false;
+    } else if (e.target.value !== '') {
+      e.target.parentElement.classList.remove('error');
+      e.target.nextElementSibling.textContent = '';
+      return true 
+    }  
+  }
+
+  firstName.addEventListener('change', (e) => {
+    allFieldsValidity[0] = validateRequired(e);
+  })
+  
+  lastName.addEventListener('change', (e) => {
+    allFieldsValidity[1] = validateRequired(e);
+  })
+  
+  email.addEventListener('change', (e) => {
+    allFieldsValidity[2] = validateRequired(e);
+  })
+  
+  phoneNum.addEventListener('change', (e) => {
+    allFieldsValidity[3] = validateRequired(e);
+  })
+  
+  password.addEventListener('change', (e) => {
+    allFieldsValidity[4] = validatePassword()
+  });
+  
+  confirmPassword.addEventListener('change', (e) => {
+    allFieldsValidity[5] = validatePassword();
+  });
+
+  function validateAllFields() {
+    inputFields.forEach(el => {
+      const event = new InputEvent('change');
+      el.dispatchEvent(event);
+    });
+    let allValid = allFieldsValidity.reduce(
+      (acc, curr) => curr && acc,
+      true
+    );
+    return allValid;
+  }
 password.onchange = validatePassword;
 confirmPassword.onkeyup = validatePassword;
