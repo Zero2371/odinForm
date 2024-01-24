6901cdce132ccf8400a14f51f6ddb3de6116cca6
Nick Sanchez <nicksanchez2371@gmail.com>
2024-01-24 13:45:55 -0600

    this works

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 const inputFields = document.querySelectorAll('input');
 const password = document.getElementById('password');
 const confirmPassword = document.getElementById('confirmPassword');
@@ -88,4 +89,137 @@ submitBtn.addEventListener('click', (e) => {
   if (validateAllFields()){
     myForm.reset();
   }
+});
+
+*/
+
+
+//--------
+
+
+
+
+const inputFields = document.querySelectorAll('input');
+const errorMsgs = document.querySelectorAll('.error-msg');
+
+const myForm = document.querySelector('form');
+const firstName = document.querySelector('#firstName');
+const lastName = document.querySelector('#lastName');
+const email = document.querySelector('#email');
+const phone = document.querySelector('#phone');
+const pwd = document.querySelector('#password');
+const retypedPwd = document.querySelector('#confirmPassword');
+
+
+const submitBtn = document.querySelector('button');
+
+let allFieldsValidity = new Array(6).fill(false);
+
+/*
+  Focused input label animation
+*/
+const onFocus = (e) => {
+  e.target.parentElement.classList.add('focused');
+}
+
+const onBlur = (e) => {
+  if (e.target.value === '') {
+    e.target.parentElement.classList.remove('focused');
+  }
+}
+
+inputFields.forEach(el => {
+  el.addEventListener('focus', onFocus)
+  el.addEventListener('blur', onBlur)
+})
+
+/*
+  Password matching logic
+*/
+const testPwdEquality = () => {
+  if (pwd.value === '' || retypedPwd === '') {
+    pwd.parentElement.classList.add('error');
+    errorMsgs[4].textContent = 'This field is required';
+    return false;
+  }
+  // display an error message if passwords do not match.
+  else if (pwd.value !== retypedPwd.value) {
+    retypedPwd.parentElement.classList.add('error');
+    errorMsgs[4].textContent = 'Passwords do not match';
+    errorMsgs[5].textContent = 'Passwords do not match';
+    return false;
+  } 
+  else if (pwd.value === retypedPwd.value) {
+  // return styling to normal if passwords match
+    pwd.parentElement.classList.remove('error');
+    retypedPwd.parentElement.classList.remove('error');
+    errorMsgs[4].textContent = '';
+    errorMsgs[5].textContent = '';
+    return true
+  }
+}
+
+/*
+  Required inputs logic
+*/
+function validateRequired(e) {
+  if (e.target.value === '') {
+    e.target.parentElement.classList.add('error');
+    e.target.nextElementSibling.textContent = 'This field is required';
+    return false;
+  } else if (e.target.value !== '') {
+    e.target.parentElement.classList.remove('error');
+    e.target.nextElementSibling.textContent = '';
+    return true 
+  }  
+}
+
+/*
+  Input event listeners
+*/
+firstName.addEventListener('change', (e) => {
+  allFieldsValidity[0] = validateRequired(e);
+})
+
+lastName.addEventListener('change', (e) => {
+  allFieldsValidity[1] = validateRequired(e);
+})
+
+email.addEventListener('change', (e) => {
+  allFieldsValidity[2] = validateRequired(e);
+})
+
+phone.addEventListener('change', (e) => {
+  allFieldsValidity[3] = validateRequired(e);
+})
+
+pwd.addEventListener('change', (e) => {
+  allFieldsValidity[4] = testPwdEquality()
+});
+
+retypedPwd.addEventListener('change', (e) => {
+  allFieldsValidity[5] = testPwdEquality();
+});
+
+/*
+  Form submit button logic
+*/
+function validateAllFields() {
+  inputFields.forEach(el => {
+    const event = new InputEvent('change');
+    el.dispatchEvent(event);
+  });
+  let allValid = allFieldsValidity.reduce(
+    (acc, curr) => curr && acc,
+    true
+  );
+  return allValid;
+}
+
+// Give user feedback if form submitted successfully
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (validateAllFields()) {
+    myForm.reset();
+  };
 })
